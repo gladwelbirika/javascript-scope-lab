5860c8d53f9f6f068eb037aa079a3bc30e14d569
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-04-16 08:57:58 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+// Write your solution in this file!
+
+// Global variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function to add a burger
+function addBurger() {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+// Block scope
+if(true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+
+    function changeFeaturedDrink() {
+        featuredDrink = 'The JavaShake';
+    }
+}
